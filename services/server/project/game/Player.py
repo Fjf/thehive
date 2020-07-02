bba722f80811445c0cabceffbf072efa7a929b14
Duncan Kampert <fjf@hotmail.nl>
2020-07-02 19:57:01 +0200

    Placeholder files for server stuff

---
+++ services/server/project/game/Player.py
@@ -0,0 +1,11 @@
+#
+# Used to track all player related information.
+# For example; if it is the player's turn, a reference to the game board, and the amount of pieces allowed to play.
+#
+
+class Player:
+    def __init__(self, board):
+        self.board = board
+
+
+
